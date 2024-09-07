2eca357eac945d863385e86ea66cfa160d9dd5e6
Paul.Shuai <2900717209@qq.com>
2024-09-07 16:04:49 +0800

    fix: add ts for node server

--- server/app.ts
+++ server/app.ts
@@ -1,16 +1,16 @@
-import { ApolloServer } from "apollo-server"
 import typeDefs from "./src/schemas/index.js"
 import resolvers from "./src/resolvers/index.js"
-// import { connectToArangoDB } from "./src/database/connectToArangoDB.js"
+import { connectToArangoDB } from "./src/database/connectToArangoDB.js"
+import { ApolloServer } from "apollo-server"
 
 const startApolloServer = async () => {
-  //   const db = await connectToArangoDB()
+  const db = await connectToArangoDB()
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: async ({ req }) => ({
+    context: async ({ req }: { req: any }) => ({
       token: req.headers.token,
-      //  db
+      db,
     }),
   })
 
